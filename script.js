834fb4765ad769492222a05e25f05ec0e3020457
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-10-17 11:56:05 +0530

    displaying the questions

--- script.js
+++ script.js
@@ -3,7 +3,26 @@ const questionObj =
       category: 'Food & Drink',
       id: 'qa-1',
       correctAnswer: 'Three',
-      answers: ['Two', 'Three ', 'Four', 'Five'],
+      options: ['Two', 'Three ', 'Four', 'Five'],
       question:
         "How many pieces of bun are in a Mcdonald's Big Mac?",
     };
+
+   const {correctAnswer, options, question} = questionObj;
+    const questionElement = document.getElementById('question');
+    const optionsElement = document.getElementById('options');
+    const scoreElement = document.getElementById('score');
+    
+    questionElement.textContent = question;
+
+
+    options.forEach((option) => {
+        const buttonElement = document.createElement('button');
+        buttonElement.textContent = option;
+        optionsElement.appendChild(buttonElement);
+    
+    })
+
+    
+
+    
