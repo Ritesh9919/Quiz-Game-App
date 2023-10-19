f2499a2010b4205acc5063237d129eeb4daaf02f
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-10-19 11:09:40 +0530

    display and select the currect answer

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const questionObj =
       category: 'Food & Drink',
       id: 'qa-1',
       correctAnswer: 'Three',
-      options: ['Two', 'Three ', 'Four', 'Five'],
+      options: ['Two', 'Three', 'Four', 'Five'],
       question:
         "How many pieces of bun are in a Mcdonald's Big Mac?",
     };
@@ -12,17 +12,32 @@ const questionObj =
     const questionElement = document.getElementById('question');
     const optionsElement = document.getElementById('options');
     const scoreElement = document.getElementById('score');
+
+    let score = 0;
+    
     
     questionElement.textContent = question;
 
-
+    
     options.forEach((option) => {
         const buttonElement = document.createElement('button');
         buttonElement.textContent = option;
         optionsElement.appendChild(buttonElement);
+        // handling event on button
+        buttonElement.addEventListener('click', ()=> {
+          if(option === correctAnswer) {
+            score++;
+          }else {
+            score = score-0.25;
+          }
+          console.log(score);
+          scoreElement.textContent = `Score:${score}`;
+          questionElement.textContent = "Quiz Completed!!";
+          optionsElement.textContent = '';
+        })
     
-    })
+    });
 
     
-
+// scoreElement.textContent = score;
     
